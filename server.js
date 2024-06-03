a2397702b6fe9db61cde60e8b1d77cd0ca702e66
Yujia <u7371774@anu.edu.au>
2024-06-03 16:48:34 +1000

    Implement user operations in server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import { authenticateUser } from './middleware/authMiddleware.js';
+import { saveGame, loadGame } from './controllers/gameController.js';
+import { register, login, logout } from './controllers/authController.js';
 import express from 'express';
 const app = express();
 app.use(express.json());
 
 import morgan from "morgan";
+
+import cookieParser from 'cookie-parser';
+app.use(cookieParser());
 
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -16,43 +20,11 @@ import mongoose from 'mongoose';
 
 mongoose.connect(process.env.MONGO_URL);
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  score: Number,
-  scorePerSecond: Number,
-  items: Array,
-});
-
-const User = mongoose.model("User", userSchema);
-
-app.post('/register', async (req, res) => {
-  // check if the username already exists
-  const existingUser = await User.findOne({ username: req.body.username });
-  if (existingUser) {
-    return res.status(400).send("Username already exists");
-  }
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  const user = new User({
-    username: req.body.username,
-    password: hashedPassword,
-    score: 0,
-    scorePerSecond: 0,
-    items: [],
-  });
-  await user.save();
-  res.status(201).send("User registered");
-});
-
-app.post('/login', async (req, res) => {
-  const user = await User.findOne({ username: req.body.username });
-  if (user && bcrypt.compare(req.body.password, user.password)) {
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    res.json({ token, username: user.username });
-  } else {
-    res.status(400).send("Invalid username or password");
-  }
-});
+app.post('/register', register);
+app.post('/login', login);
+app.get('/logout', logout);
+app.post('/save', authenticateUser, saveGame);
+app.get('/load', authenticateUser, loadGame);
 
 const port = process.env.PORT || 5100;
 
