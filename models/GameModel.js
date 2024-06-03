5185911fc609d29a660b8f8fd621126d46a7d5f5
Yujia <u7371774@anu.edu.au>
2024-06-03 16:08:14 +1000

    Connect game model with user

--- models/GameModel.js
+++ models/GameModel.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
+import { type } from "os";
 
 const GameSchema = new mongoose.Schema({
   score: Number,
   scorePerSecond: Number,
   items: Array,
+  ownedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }
 });
 
 export default mongoose.model('Game', GameSchema);
