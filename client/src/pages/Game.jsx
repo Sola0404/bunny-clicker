af0bbe12ceab26c7864ce1e61d0fcb86ae8fb853
Yujia <u7371774@anu.edu.au>
2024-06-04 15:43:54 +1000

    Refactor Game code

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -10,78 +10,46 @@ import customFetch from "../utils/customFetch";
 import { redirect } from "react-router-dom";
 
 const Game = () => {
-	const [score, setScore] = useState(() => {
+	const loadGameFromLocalStorage = () => {
 		const saveGame = localStorage.getItem("saveGame");
-		return saveGame ? JSON.parse(saveGame).score : 0;
-	});
-	const [scorePerSecond, setScorePerSecond] = useState(() => {
-		const saveGame = localStorage.getItem("saveGame");
-		return saveGame ? JSON.parse(saveGame).scorePerSecond : 0;
-	});
-	const [items, setItems] = useState(() => {
-		const saveGame = localStorage.getItem("saveGame");
-		return saveGame ? JSON.parse(saveGame).items : itemsData;
-	});
+		if (saveGame) {
+			return JSON.parse(saveGame);
+		}
+		return { score: 0, scorePerSecond: 0, items: itemsData };
+	};
+
+	const initialGameState = loadGameFromLocalStorage();
+
+	const [score, setScore] = useState(initialGameState.score);
+	const [scorePerSecond, setScorePerSecond] = useState(
+		initialGameState.scorePerSecond
+	);
+	const [items, setItems] = useState(initialGameState.items);
 
 	const clickingPower = 1;
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setScore((prevScore) => prevScore + scorePerSecond);
-		}, 1000);
-		return () => clearInterval(interval);
-	}, [scorePerSecond]);
-
-	useEffect(() => {
-		const newScorePerSecond = items.reduce(
-			(acc, item) => acc + item.amount * item.increment,
-			0
-		);
-		setScorePerSecond(newScorePerSecond);
-	}, [items]);
-
 	const loadGameFromServer = async () => {
 		try {
-			// if there is a user logged in
 			const response = await customFetch.get("/load");
 			const { score, scorePerSecond, items } = response.data;
-			console.log(response.data);
 			setScore(score);
 			setScorePerSecond(scorePerSecond);
 			setItems(items);
 		} catch (error) {
-			// if not login
 			redirect("/login");
 		}
 	};
 
-	useEffect(() => {
-		// Read from local storage in default
-		const loadGame = () => {
-			const saveGame = localStorage.getItem("saveGame");
-			if (saveGame) {
-				const saveData = JSON.parse(saveGame);
-				setScore(saveData.score);
-				setItems(saveData.items);
-			}
-		};
-		loadGame();
-	}, []);
-
-	useEffect(() => {
-		const saveGame = () => {
-			localStorage.setItem(
-				"saveGame",
-				JSON.stringify({ score, scorePerSecond, items })
-			);
-		};
-		saveGame();
-	}, [score, items]);
+	const saveGameToLocalStorage = () => {
+		localStorage.setItem(
+			"saveGame",
+			JSON.stringify({ score, scorePerSecond, items })
+		);
+	};
 
 	const saveGameToServer = async () => {
 		try {
 			const requestBody = { score, scorePerSecond, items };
-			console.log(requestBody);
 			await customFetch.post("/save", requestBody);
 		} catch (error) {
 			console.error(error);
@@ -90,7 +58,7 @@ const Game = () => {
 	};
 
 	const resetGame = () => {
-		if (confirm("Are you sure you want to reset the game?")) {
+		if (window.confirm("Are you sure you want to reset the game?")) {
 			localStorage.removeItem("saveGame");
 			setScore(0);
 			setScorePerSecond(0);
@@ -98,13 +66,40 @@ const Game = () => {
 		}
 	};
 
+	// Update scorePerSecond every time items change
+	useEffect(() => {
+		const newScorePerSecond = items.reduce(
+			(acc, item) => acc + item.amount * item.increment,
+			0
+		);
+		setScorePerSecond(newScorePerSecond);
+	}, [items]);
+
+	// Update score every second
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setScore((prevScore) => prevScore + scorePerSecond);
+		}, 1000);
+		return () => clearInterval(interval);
+	}, [scorePerSecond]);
+
+	// Load game data from local storage in default
+	useEffect(() => {
+		loadGameFromLocalStorage();
+	}, []);
+
+	// Save game data to local storage when score or items change
+	useEffect(() => {
+		saveGameToLocalStorage();
+	}, [score, items]);
+
 	const addToScore = (value) => {
 		setScore((prevScore) => prevScore + value);
 	};
 
 	const updateItems = (updatedItem) => {
-		setItems(
-			items.map((item) => (item.id === updatedItem.id ? updatedItem : item))
+		setItems((prevItems) =>
+			prevItems.map((item) => (item.id === updatedItem.id ? updatedItem : item))
 		);
 	};
 
