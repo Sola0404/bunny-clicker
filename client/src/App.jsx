3d87f617edae92f2a6e617afba3fad23578284fd
Yujia <u7371774@anu.edu.au>
2024-06-04 14:19:38 +1000

    Implement loader and saver

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Game, Register, Login, Error } from "./pages";
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
+import { loader as gameLoader } from "./pages/Game";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Game />,
 		errorElement: <Error />,
+		loader: gameLoader,
 	},
 	{
 		path: "/register",
--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -6,20 +6,38 @@ import {
 	ShopContainer,
 	UpgradeContainer,
 } from "../components";
+import customFetch from "../utils/customFetch";
+import { redirect, useLoaderData } from "react-router-dom";
+
+export const loader = async () => {
+	try {
+		// if there is a user logged in
+		const response = await customFetch.get("/load");
+		const { score, scorePerSecond, items } = response.data;
+		console.log(response.data);
+		return { score, scorePerSecond, items };
+	} catch (error) {
+		// Not login
+		// if there is game data in local storage
+		const saveGame = localStorage.getItem("saveGame");
+		if (saveGame) {
+			return JSON.parse(saveGame);
+		}
+		// if there is no game data in local storage
+		return { score: 0, scorePerSecond: 0, items: itemsData };
+	}
+};
 
 const Game = () => {
-	const [score, setScore] = useState(() => {
-		const saveGame = localStorage.getItem("saveGame");
-		return saveGame ? JSON.parse(saveGame).score : 0;
-	});
-	const [scorePerSecond, setScorePerSecond] = useState(() => {
-		const saveGame = localStorage.getItem("saveGame");
-		return saveGame ? JSON.parse(saveGame).scorePerSecond : 0;
-	});
-	const [items, setItems] = useState(() => {
-		const saveGame = localStorage.getItem("saveGame");
-		return saveGame ? JSON.parse(saveGame).items : itemsData;
-	});
+	const {
+		score: initialScore,
+		scorePerSecond: initialScorePerSecond,
+		items: initialItems,
+	} = useLoaderData();
+
+	const [score, setScore] = useState(initialScore);
+	const [scorePerSecond, setScorePerSecond] = useState(initialScorePerSecond);
+	const [items, setItems] = useState(initialItems);
 
 	const clickingPower = 1;
 
@@ -39,18 +57,6 @@ const Game = () => {
 	}, [items]);
 
 	useEffect(() => {
-		const loadGame = () => {
-			const saveGame = localStorage.getItem("saveGame");
-			if (saveGame) {
-				const saveData = JSON.parse(saveGame);
-				setScore(saveData.score);
-				setItems(saveData.items);
-			}
-		};
-		loadGame();
-	}, []);
-
-	useEffect(() => {
 		const saveGame = () => {
 			localStorage.setItem(
 				"saveGame",
@@ -59,6 +65,15 @@ const Game = () => {
 		};
 		saveGame();
 	}, [score, items]);
+
+	const saveGameToServer = async () => {
+		try {
+			await customFetch.post("/save", { score, scorePerSecond, items });
+		} catch (error) {
+			console.error(error);
+			redirect("/login");
+		}
+	};
 
 	const resetGame = () => {
 		if (confirm("Are you sure you want to reset the game?")) {
@@ -87,6 +102,9 @@ const Game = () => {
 				<br />
 				<ClickerContainer addToScore={addToScore} clickingPower={clickingPower} />
 				<div className="section-footer">
+					<button className="reset-btn" onClick={saveGameToServer}>
+						Save Game
+					</button>
 					<button className="reset-btn" onClick={resetGame}>
 						Reset Game
 					</button>
