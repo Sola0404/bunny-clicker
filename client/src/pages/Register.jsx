0da4f33e506f68c734bcd490228f433a9051934b
Yujia <u7371774@anu.edu.au>
2024-06-04 11:25:26 +1000

    Install concurrently and add concurrent command

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,30 @@
-import { Link } from "react-router-dom";
+import { Form, Link, redirect } from "react-router-dom";
+import axios from "axios";
+import { SubmitBtn } from "../components";
+
+export const action = async ({ request }) => {
+	const formData = await request.formData();
+	const data = Object.fromEntries(formData);
+	try {
+		await axios.post("http://localhost:5100/register", data);
+		return redirect("/login");
+	} catch (error) {
+		return error;
+	}
+};
 
 const Register = () => {
 	return (
 		<div>
-			<div>Register</div>
-			<Link to="/login">Login</Link>
+			<Form method="post">
+				<div>Register</div>
+				<input type="text" name="username" placeholder="Username" />
+				<input type="password" name="password" placeholder="Password" />
+				<SubmitBtn />
+			</Form>
+			<p>
+				Already having an account? <Link to="/login">Login</Link>
+			</p>
 		</div>
 	);
 };
