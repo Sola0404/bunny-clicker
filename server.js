89a2b10513cee76888f0974b1f9ca0bb0ae571d3
Yujia <u7371774@anu.edu.au>
2024-06-05 15:45:16 +1000

    Deploy app in Render

--- server.js
+++ server.js
@@ -1,54 +1,57 @@
 import "express-async-errors";
+import express from 'express';
+import morgan from "morgan";
+import cookieParser from 'cookie-parser';
+import * as dotenv from "dotenv";
+import mongoose from 'mongoose';
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Import middlewares and controllers
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 import { authenticateUser } from './middleware/authMiddleware.js';
 import { saveGame, loadGame } from './controllers/gameController.js';
 import { register, login, logout } from './controllers/authController.js';
-import express from 'express';
-import path from 'path';
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { validateLoginInput, validateRegisterInput } from './middleware/validationMiddleware.js';
 
+// Initialize app
+dotenv.config();
 const app = express();
 app.use(express.json());
-
-import morgan from "morgan";
-
-import cookieParser from 'cookie-parser';
 app.use(cookieParser());
 
-import * as dotenv from "dotenv";
-dotenv.config();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.static(path.resolve(__dirname, "./client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
-});
-
-import mongoose from 'mongoose';
-import { validateLoginInput, validateRegisterInput } from './middleware/validationMiddleware.js';
-
+// Define API routes
 app.post('/api/register', validateRegisterInput, register);
 app.post('/api/login', validateLoginInput, login);
 app.get('/api/logout', logout);
 app.post('/api/save', authenticateUser, saveGame);
 app.get('/api/load', authenticateUser, loadGame);
 
+// Error handling middleware
 app.use(errorHandlerMiddleware);
 
+// Static file serving
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
+
+// Catch-all route to serve the React app for client-side routing
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+});
+
+// Connect to MongoDB and start server
 const port = process.env.PORT || 5100;
 
 try {
   mongoose.connect(process.env.MONGO_URL);
   app.listen(port, () => {
-    console.log(`server running on Port ${port}`);
+    console.log(`Server running on Port ${port}`);
   });
 } catch (error) {
   console.log(error);
   process.exit(1);
 }
-
